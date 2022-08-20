81f13975ccf2314647dcf9dfb7984669d19a4bc1
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2022-08-20 18:38:29 -0300

    corrige identação da main

--- main.cpp
+++ main.cpp
@@ -2,29 +2,31 @@
 #include <cstdlib>
 #include <vector>
 #include <GL/glut.h>
+#include "textures/object.h"
 
 // textura
 #define STB_IMAGE_IMPLEMENTATION
-#include "textures/object.h"
 
 // paletas de cores
-#define COLOR_BACKGROUND  glClearColor(0.00392, 0.05098, 0.00392, 0.000000)
-#define COLOR_FONT_MENU   glColor3f(1.000000, 1.000000, 1.000000)
-#define COLOR_BLUE_MENU   glColor3f(0.000000, 0.362000, 0.832000)
-#define COLOR_GREEN		  glColor3ub(6, 133, 90);
-#define LIGHT_GREEN		  glColor3ub(142, 191, 37);
-#define SLATE_BLUE 	      glColor3f(0.000000, 0.498000, 1.000000)
-#define STEEL_BLUE 	      glColor3f(0.137300, 0.419600, 0.556900)
-#define AQUAMARINE 	      glColor3f(0.439200, 0.858800, 0.576500)
-#define FIREBRICK         glColor3f(0.556900, 0.137300, 0.137300)
-#define ORCHID 	   	      glColor3f(0.858800, 0.439200, 0.858800)
-#define CORAL 	   	      glColor3f(1.000000, 0.498000, 0.000000)
-#define DARK_SLATE_BG     glClearColor(0.419600, 0.137300, 0.556900, 0.000000)
-#define MIDNIGHT_BLUE_BG  glClearColor(0.184300, 0.184300, 0.309800, 0.000000)
-#define BROWN_BG	      glClearColor(0.647100, 0.164700, 0.164700, 0.000000)
-#define GOLD_BG    		  glClearColor(0.800000, 0.498000, 0.196100, 0.000000)
-#define VIOLET_BG 	      glClearColor(0.309800, 0.184300, 0.309800, 0.000000)
-#define DARK_ORCHID_BG 	  glClearColor(0.600000, 0.196100, 0.800000, 0.000000)
+#define LIGHT_GREEN			glColor3ub(142, 191, 37);
+#define COLOR_GREEN			glColor3ub(6, 133, 90);
+
+#define COLOR_FONT_MENU		glColor3f(1.000000, 1.000000, 1.000000)
+#define COLOR_BLUE_MENU		glColor3f(0.000000, 0.362000, 0.832000)
+#define SLATE_BLUE			glColor3f(0.000000, 0.498000, 1.000000)
+#define STEEL_BLUE			glColor3f(0.137300, 0.419600, 0.556900)
+#define AQUAMARINE			glColor3f(0.439200, 0.858800, 0.576500)
+#define FIREBRICK			glColor3f(0.556900, 0.137300, 0.137300)
+#define ORCHID				glColor3f(0.858800, 0.439200, 0.858800)
+#define CORAL				glColor3f(1.000000, 0.498000, 0.000000)
+
+#define COLOR_BACKGROUND	glClearColor(0.003920, 0.050980, 0.003920, 0.000000)
+#define DARK_SLATE_BG		glClearColor(0.419600, 0.137300, 0.556900, 0.000000)
+#define MIDNIGHT_BLUE_BG	glClearColor(0.184300, 0.184300, 0.309800, 0.000000)
+#define BROWN_BG			glClearColor(0.647100, 0.164700, 0.164700, 0.000000)
+#define GOLD_BG				glClearColor(0.800000, 0.498000, 0.196100, 0.000000)
+#define VIOLET_BG			glClearColor(0.309800, 0.184300, 0.309800, 0.000000)
+#define DARK_ORCHID_BG		glClearColor(0.600000, 0.196100, 0.800000, 0.000000)
 
 using namespace std;
 
